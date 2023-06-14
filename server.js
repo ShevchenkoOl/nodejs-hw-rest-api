377488a05451bac73b98d3f534c71216e3ce52fd
Oleksii Shevchenko <uzlabini@gmail.com>
2023-06-14 20:34:29 +0200

    update app.js&server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express');
+
 const app = require('./app')
 
-app.use(express.static('./'))
+
 app.listen(3000, () => {
   console.log("Server running. Use our API on port: 3000")
 })
